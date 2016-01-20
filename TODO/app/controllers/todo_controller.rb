b0a82cc5c99059e4a1efdb5d5da38250e476ce31
ElstJ-AP <jonas.elst@hotmail.com>
2016-01-20 15:25:43 +0100

    Filter methode toegevoegd
    Nu kan er gefilterd worden op prioriteit. Dit is de laatste commit.

--- TODO/app/controllers/todo_controller.rb
+++ TODO/app/controllers/todo_controller.rb
@@ -1,7 +1,6 @@
 class TodoController < ApplicationController
 	def create
 		@url = request.original_url
-		@urlExample = "http://localhost:3000/todo/create?ingavedatum=20/01/2016&einddatum=27/01/2016&prioriteit=1&beschrijving=Boter kopen&status=todo"
 
 		@ingavedatum = params[:ingavedatum]
 		@einddatum = params[:einddatum]
@@ -16,7 +15,13 @@ class TodoController < ApplicationController
 		@response = @db.save_doc( { 'ingavedatum' => @ingavedatum, 'einddatum' => @einddatum, 'prioriteit' => @prioriteit, 'beschrijving' => @beschrijving, 'status' => @status })
 
 		@json = { :ingavedatum => @ingavedatum, :einddatum => @einddatum, :prioriteit => @prioriteit, :beschrijving => @beschrijving, :status => @status }.to_json
+	end
 
+	def filter
+		@url = request.original_url
 
+		@prioriteit = params[:prioriteit]
+
+		@todos = Todo.where(prioriteit: @prioriteit)
 	end
 end
--- TODO/config/routes.rb
+++ TODO/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'todo/create' => 'todo#create'
+  get 'todo/filter' => 'todo#filter'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
