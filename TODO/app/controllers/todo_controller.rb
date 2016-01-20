a0a3756bb6f4f13488afc78210ed68530b9b3d31
ElstJ-AP <jonas.elst@hotmail.com>
2016-01-20 14:52:52 +0100

    first part of the question
    application able to get parameters from url and to turn them into a json

--- TODO/app/controllers/todo_controller.rb
+++ TODO/app/controllers/todo_controller.rb
@@ -1,2 +1,16 @@
 class TodoController < ApplicationController
+	def create
+		@url = request.original_url
+		@urlExample = "http://localhost:3000/todo/create?ingavedatum=20/01/2016&einddatum=27/01/2016&prioriteit=1&beschrijving=Boter kopen&status=todo"
+
+		@ingavedatum = params[:ingavedatum]
+		@einddatum = params[:einddatum]
+		@prioriteit = params[:prioriteit]
+		@beschrijving = params[:beschrijving]
+		@status = params[:status]
+
+		@json = { :ingavedatum => @ingavedatum, :einddatum => @einddatum, :prioriteit => @prioriteit, :beschrijving => @beschrijving, :status => @status }.to_json
+
+
+	end
 end
